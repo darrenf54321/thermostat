2b19a511b622d3d49569015c00e765680231bc3e
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-26 15:26:11 +0100

    added weather API

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -34,21 +34,20 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
   function displayWeather(city) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
     var token = '&appid=1d4c0b4fffcce18c6d389b6d114d489b';
     var units = '&units=metric';
     $.get(url + token + units, function(data) {
       $('#current-temperature').text(data.main.temp);
-    };
-  };
-    displayWeather('London');
-    $('#select-city').submit(function(event) {
-      event.preventDefault();
-      var city = $('#current-city').val();
-      displayWeather(city);
     });
-
+  };
 });
-
-//1d4c0b4fffcce18c6d389b6d114d489b
