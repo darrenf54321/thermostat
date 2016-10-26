6c82e8f293b9d6308fb940f6684614c979f0468b
Darrenf <darrenf54321@gmail.com>
2016-10-26 15:08:44 +0100

    city temp ongoing, failing

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -33,4 +33,22 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
   };
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=1d4c0b4fffcce18c6d389b6d114d489b';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    };
+  };
+    displayWeather('London');
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+    });
+
 });
+
+//1d4c0b4fffcce18c6d389b6d114d489b
